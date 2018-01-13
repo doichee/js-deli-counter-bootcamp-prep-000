bed09de0d4277691234293cc5f05e7dca9ffaf75
Ben Deutsch <ben.deutschiii@gmail.com>
2018-01-13 20:29:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,17 +14,13 @@ function nowServing(katzDeliLine) {
 };
 
 function currentLine(katzDeliLine) {
-  if(katzDeliLine.length > 0); {
-    var line = "The line is currently: ";
-    for (var i = 1; i <= katzDeliLine.length; i++) {
-      if (i === katzDeliLine.length) {
-        line += `${i}. ${katzDeliLine[i-1]}`;
-      } else {
-        line += `${i}. ${katzDeliLine[i-1]}, `;
-      }
-      return line;
+  if(katzDeliLine.length === 0) {
+    return "The line is currently empty."  
+  } else {
+    var line = [];
+    for (let i = 0; i < katzDeliLine.length; i++) {
+      line.push(`${i + 1}. ${katzDeliLine[i]}`);
     }
-      return 'The line is currently empty.';
-
+    return "The line is currently: " + line;
   }
 }
