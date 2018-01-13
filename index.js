f3d125e0602105eee98fc2f7bf41b8afc975cd38
Ben Deutsch <ben.deutschiii@gmail.com>
2018-01-13 20:01:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,4 +2,13 @@ var katzDeliLine = [];
 function takeANumber(katzDeliLine, person) {
   katzDeliLine.push(person);
   return `Welcome, ${person}. You are number ${katzDeliLine.length} in line.`;
+};
+
+function nowServing(katzDeliLine) {
+  if(katzDeliLine.length === 0) {
+    return 'There is nobody waiting to be served!';
+  } else {
+    var firstName = katzDeliLine.shift();
+    return `Currently serving ${firstName}.`;
+  }
 }
