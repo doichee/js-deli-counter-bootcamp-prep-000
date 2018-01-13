bdea0dfb250d7b3661ef71193b79a8cf2140098d
Ben Deutsch <ben.deutschiii@gmail.com>
2018-01-13 20:15:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,4 +11,20 @@ function nowServing(katzDeliLine) {
     var firstName = katzDeliLine.shift();
     return `Currently serving ${firstName}.`;
   }
+};
+
+function currentLine(katzDeliLine) {
+  if(katzDeliLine.length > 0); {
+    var line = "The line is currently: ";
+    for (var i = 1; i <= katzDeliLine.length; i++) {
+      if (i === katzDeliLine.length) {
+        line += `${i}. ${katzDeliLine[i-1]}`;
+      } else {
+        line += `${i}. ${katzDeliLine[i-1]}, `;
+      }
+      return line;
+    } else {
+      return 'The line is currently empty.';
+    }
+  }
 }
