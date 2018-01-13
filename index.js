1d00f72d06fed3bb0fc0ff4b03f8e167d95713d6
Ben Deutsch <ben.deutschiii@gmail.com>
2018-01-13 20:29:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ function nowServing(katzDeliLine) {
 
 function currentLine(katzDeliLine) {
   if(katzDeliLine.length === 0) {
-    return "The line is currently empty."  
+    return "The line is currently empty."
   } else {
     var line = [];
     for (let i = 0; i < katzDeliLine.length; i++) {
